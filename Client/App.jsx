f450415c4552b2f048946833f746e817e61eb14e
Nrking913 <nrking913@comcast.net>
2023-06-14 12:31:08 -0700

    Added login page styling

--- Client/App.jsx
+++ Client/App.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
 import Login from './Components/login.jsx';
+import MainPage from './Components/MainPage.jsx'
+
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(true);
   
   return (
-    <div>
-      <p>WE GOT THIS!</p>
+
+    <div className="mainPage">
       {/* loggedIn ? loginpage:querypage */}
-      {loggedIn && <Login setLoggedIn={setLoggedIn}/>}
+      {loggedIn && (
+        <div className="introPage">
+          <div className='navBar'></div>
+            {/* <h1 className='loginHeader'>Welcome to Twerp</h1> */}
+          <Login setLoggedIn={setLoggedIn}/>
+        </div>
+      )}
+
+      {!loggedIn && (
+          <MainPage/>
+      )}
     </div>
   );
 };
